4c19b2f0d6a3579ca5cc3f5b7f60eccab122efaf
aoubak <aoubak01@gmail.com>
2025-05-31 17:57:22 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Header.jsx 	modified:   src/components/Hero.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,63 @@
-import React from "react";
+import React, { use, useState } from "react";
+import { IoMdMenu, IoMdClose } from "react-icons/io";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header>
-      <nav className="bg-gray-800 mx-auto">
+    <header className="fixed top-0 left-0 w-full z-50">
+      <nav className="bg-gray-800 mx-auto relative ">
         <div className="container mx-auto flex justify-between items-center bg-gray-800  text-white p-4 ">
           <div className="text-2xl font-bold">
             <a href="/" className="hover:text-gray-400">
               AOUBAK
             </a>
           </div>
-          <ul className="flex space-x-4 hidden md:flex">
-            <li>
-              <a className="hover:text-gray-400" href="/">Home</a>
+          <ul
+            className={`absolute top-full h-screen w-full bg-gray-700 p-4 transition-all duration-700 ease-in-out md:static md:h-auto md:w-auto md:flex md:space-x-4 md:p-0 md:bg-transparent ${
+              isOpen ? "left-0" : "-left-full"
+            }`}
+          >
+            <li className="w-full md:w-auto bg-gray-600 md:bg-transparent mb-2 md:mb-0 rounded-md md:rounded-none ">
+              <a className="hover:text-gray-400 block p-2" href="/">
+                Home
+              </a>
             </li>
-            <li >
-              <a className="hover:text-gray-400" href="/about">About</a>
+            <li className="w-full md:w-auto bg-gray-600 md:bg-transparent mb-2 md:mb-0 rounded-md ">
+              <a className="hover:text-gray-400 block p-2" href="/about">
+                About
+              </a>
             </li>
-            <li >
-              <a className="hover:text-gray-400" href="/contact">Contact</a>
+            <li className="w-full md:w-auto bg-gray-600 md:bg-transparent mb-2 md:mb-0 rounded-md ">
+              <a className="hover:text-gray-400 block p-2" href="/contact">
+                Contact
+              </a>
             </li>
-            <li >
-              <a className=" hover:bg-indigo-500 bg-indigo-600 rounded px-2 py-2" href="/login">Login</a>
+            <li className="w-full md:w-auto">
+              <a
+                className=" hover:bg-indigo-500 bg-indigo-600 rounded px-2 py-2  mb-2 md:mb-0 block"
+                href="/login"
+              >
+                Login
+              </a>
             </li>
-            <li >
-              <a className="hover:bg-indigo-500 bg-indigo-600 rounded px-2 py-2" href="/register">Register</a>
+            <li className="w-full md:w-auto">
+              <a
+                className="hover:bg-indigo-500 bg-indigo-600 rounded px-2 py-2 block"
+                href="/register"
+              >
+                Register
+              </a>
             </li>
           </ul>
+          <div className="md:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="md:hidden text-3xl"
+            >
+              {isOpen ? <IoMdClose size={24} /> : <IoMdMenu size={24} />}
+            </button>
+          </div>
         </div>
       </nav>
     </header>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,17 +3,15 @@ import heroImage from '../assets/images/hero.png'; // Adjust the path as necessa
 
 export default function Hero() {
 return (
-    <div>
+    <div className='mt-16 md:mb-20'>
         <section className="bg-linear-65 bg-gradient-to-r from-indigo-800 via-purple-500 to-pink-500 text-white py-16 md:py-20">
             <div className="container mx-auto p-4 flex flex-col md:flex-row justify-center items-center gap-8">
-                <div className="flex-1 w-full text-center md:text-left mb-8 md:mb-0">
-                    <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4">Hi, Welcome to </h1>
-                    <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4">My Website</h1>
+                <div className="flex-1 w-full text-left md:text-left mb-8 md:mb-0">
+                    <h1 className="text-4xl leading-none sm:text-4xl md:text-8xl font-bold mb-2 ">Hi, Welcome </h1>
+                    <h1 className="text-4xl leading-none sm:text-4xl md:text-6xl font-bold mb-2">To My Website</h1>
                     <p className="text-base sm:text-lg mb-8">This is a simple hero section with a call to action.</p>
-                    <a
-                        href="/get-started"
-                        className="bg-gray-900 hover:bg-indigo-700 text-white font-bold py-2 px-6 rounded inline-block"
-                    >
+
+                    <a href="/get-started" className="bg-gray-900 hover:bg-indigo-700 text-white font-bold py-2 px-6 rounded inline-block" >
                         Get Started
                     </a>
                 </div>
